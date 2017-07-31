3e7f11a95d7231555775a9e9b4580feed6818006
Victor Allen <vamuigua@gmail.com>
2017-07-31 16:31:48 +0300

    Update destroy action for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,32 @@ class TasksController < ApplicationController
       end
     end
 
+    #controller action to handle the edit page
+    def edit
+      @list = List.find(params[:list_id])
+      @task = Task.find(params[:id])
+      render :edit
+    end
+
+    #action to update the particular task according to its id
+    def update
+      @list = List.find(params[:list_id])
+      @task = @list.tasks.find(params[:id])
+      if @task.update(task_params)
+        redirect_to list_path(@list)
+      else
+        render :edit
+      end
+    end
+
+    #action controller to delete a particular task for a list
+    def destroy
+      @list = List.find(params[:list_id])
+      @task = @list.tasks.find(params[:id])
+      @task.destroy
+      redirect_to list_path(@list)
+    end
+
    #The private method used by strong parameters,
    #permits only designated params to be passed for our model
    #used by create and update actions 
