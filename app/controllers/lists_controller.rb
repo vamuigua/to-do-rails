c3b40da67c184a65e2fe2b790b9f9ffe0686ec00
Victor Allen <vamuigua@gmail.com>
2017-07-31 12:16:27 +0300

    added action for creating a new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,17 @@
 class ListsController < ApplicationController
+  #index action to return all lists
   def index
     @lists = List.all
     render :index
   end
+  #action to show details of a particular list
    def show
       @list = List.find(params[:id])
       render :show
     end
+   #action to create a new list
+   def new
+      @list = List.new
+      render :new
+    end 
 end
