e490d673cd1309ea5e956e5494f13e654e221804
Victor Allen <vamuigua@gmail.com>
2017-07-31 15:15:20 +0300

    Added controller for creating a new task for a list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,27 @@
 class TasksController < ApplicationController
+    #controller to render page for creating a task for a list
     def new
       @list = List.find(params[:list_id])
       @task = @list.tasks.new
+    end
+
+    #controller for creating a task for a particular list
+    def create
+      @list = List.find(params[:list_id])
+      @task = @list.tasks.new(task_params)
+      if @task.save
+        redirect_to list_path(@task.list)
+      else
+        render :new
+      end
+    end
+
+   #The private method used by strong parameters,
+   #permits only designated params to be passed for our model
+   #used by create and update actions 
+  private
+    def task_params
+      params.require(:task).permit(:description)
     end
   end
   
