6b08b05097ccaffd0a0d972eaa7663f07240a15a
Victor Allen <vamuigua@gmail.com>
2017-08-01 11:45:14 +0300

    Removed unneccessary renders of same controller action name

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,17 +2,14 @@ class ListsController < ApplicationController
   #index action to return all lists
   def index
     @lists = List.all
-    render :index
   end
   #action to show details of a particular list
    def show
       @list = List.find(params[:id])
-      render :show
     end
    #action to render a new list form
    def new
       @list = List.new
-      render :new
     end
     #action to create a new list
    def create
@@ -30,7 +27,6 @@ class ListsController < ApplicationController
     #controller action to handle the edit page
     def edit
       @list = List.find(params[:id])
-      render :edit
     end
 
     #action to update the particular list according to its id
